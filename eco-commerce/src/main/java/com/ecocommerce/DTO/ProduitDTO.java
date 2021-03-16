4f8a6a94b94511955befabccee7b487cf38ba6ce
Aeckeline Ravalomanda <aeckeline@gmail.com>
2021-03-16 23:43:45 +0100

    Création des entités Panier et Produit et leurs DAO et DTO.

---
+++ eco-commerce/src/main/java/com/ecocommerce/DTO/ProduitDTO.java
@@ -0,0 +1,22 @@
+package com.ecocommerce.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ProduitDTO {
+	
+	private Long id;
+	private String nom;
+	private String description;
+	private String image;
+
+}
---
+++ eco-commerce/src/main/java/com/ecocommerce/Entity/Produit.java
@@ -0,0 +1,59 @@
+package com.ecocommerce.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//classe correspondant à la table produit dans la base de données
+
+@Entity
+public class Produit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "produit_seq")
+	private Long id;
+	private String nom;
+	private String description;
+	private String image;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Produit(Long id, String nom, String description, String image) {
+		super();
+		id = id;
+		this.nom = nom;
+		this.description = description;
+		this.image = image;
+	}
+
+}
