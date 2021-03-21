c04398eddad3c626eaedfaf8d49902d1b590e8c3
Aeckeline Ravalomanda <aeckeline@gmail.com>
2021-03-21 23:51:39 +0100

    Injecte les services panier et users-connecte dans le composant panier.

--- eco-commerce-front/src/app/panier/panier.component.ts
+++ eco-commerce-front/src/app/panier/panier.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { panier } from '../Models/panier';
+import { UsersConnecteServiceService } from '../services/users-connecte-service.service';
+import { PanierService } from '../services/panier.service';
 
 @Component({
   selector: 'app-panier',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanierComponent implements OnInit {
 
-  constructor() { }
+  panierUtilisateur: panier;
+  
+  constructor(
+	private userService: UsersConnecteServiceService,
+	private panierService: PanierService) { }
 
   ngOnInit(): void {
+	//TODO appeler panierService avec l'id de l'utilisateur connecté
+
   }
 
 }
