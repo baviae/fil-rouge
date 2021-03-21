d64032b67c098c995a3cf5162da5c01d05a3aba9
Aeckeline Ravalomanda <aeckeline@gmail.com>
2021-03-21 22:04:26 +0100

    Supprime import RouterModule non utilisé.

--- eco-commerce-front/src/app/app.module.ts
+++ eco-commerce-front/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
