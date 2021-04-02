3904e39ad0502db8e763ab4ca989538ea3e8ca11
HOUMADI Badrane <badrane976@gmail.com>
2021-04-02 16:10:24 +0200

    ajout exec pour le lancement auto des install

---
+++ EXEC.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+# update all packages
+sudo apt update -y
+sudo apt upgrade -y
+
+# install git and wget
+sudo apt install git wget
+
+# added monitoring tools
+sudo apt install -y htop iotop iftop
+
+# installation python3
+sudo apt install -y python3
+sudo apt install python3-pip -y
+
+# install ansible last version
+pip3 install natsort
+pip3 install wheel
+pip3 install --upgrade pip
+pip3 install ansible
+ansible --version
+
+# run playbook installation de docker
+ansible-playbook -i inventory ./ansible/playbook.yml
+
+# run ansible playbook install Environnement et déploiement des applications
+ansible-playbook -i inventory EnvInstallation.yml
+
