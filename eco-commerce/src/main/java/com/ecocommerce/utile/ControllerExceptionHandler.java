425b252ab2f94e5a40f5f7775669c44a9f64426a
HOUMADI Badrane <badrane976@gmail.com>
2021-04-14 11:22:36 +0200

    ajout de reponse entity dans le handler

--- eco-commerce/src/main/java/com/ecocommerce/utile/ControllerExceptionHandler.java
+++ eco-commerce/src/main/java/com/ecocommerce/utile/ControllerExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.ecocommerce.utile;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 
 @ControllerAdvice
@@ -12,8 +16,14 @@ public class ControllerExceptionHandler {
 	private static Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 	
 	  @ExceptionHandler(Exception.class)
-	  public void resourceException(Exception ex) {
+	  public ResponseEntity<Object> resourceException(Exception ex, WebRequest request) {
 			logger.error("[ "+ex.toString()+" ], cause : => " +ex.getCause() +" : " + ex.getMessage());
+			
+			if (ex instanceof IOException) {
+				return ResponseEntity.badRequest().body(ex.getMessage());
+			} else {
+				return ResponseEntity.badRequest().body(ex.getMessage());
+			}
 	  }
 
 }
